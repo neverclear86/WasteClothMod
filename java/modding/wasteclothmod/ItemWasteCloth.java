5dafe7bed8abf4d2024ab124559b7a5c35523a19
neverclear86 <neverclear86sab@gmail.com>
2015-05-01 22:32:16 +0900

    ソース

--- java/modding/wasteclothmod/ItemWasteCloth.java
+++ java/modding/wasteclothmod/ItemWasteCloth.java
@@ -18,7 +18,7 @@ public class ItemWasteCloth extends Item
 	public ItemWasteCloth()
 	{
 		super();
-		this.setCreativeTab(WasteClothModCore.tabWasteCloth)        //クリエタブに追加
+		this.setCreativeTab(WasteClothModCore.tabWasteCloth) 	       //クリエタブに追加
 			.setUnlocalizedName("WasteCloth")                       //システム名登録
 			.setTextureName("WasteClothMod:waste_cloth")            //テクスチャ登録
 			.setHasSubtypes(true)                                   //ダメージ値を使う
@@ -36,20 +36,20 @@ public class ItemWasteCloth extends Item
 		/*onItemUseFirstは右クリック時にブロック側の処理より先に処理される
 		  onItemUseだとブロックの後で処理される	（タートルだとGUIが開く）*/
 
-		TileEntity tile = world.getTileEntity(x, y, z);				/*リフレクション*/
-																	/*無理やりメソッドを呼び出すっぽい*/
+		TileEntity tile = world.getTileEntity(x, y, z);             /*リフレクション*/
+                                                                    /*無理やりメソッドを呼び出すっぽい*/
 		if (tile != null) {
 		    try {
 		        Method methodGetAccess = tile.getClass().getMethod("getAccess");
 		        ITurtleAccess turtleAccess = (ITurtleAccess)methodGetAccess.invoke(tile);
 
-		        if (!world.isRemote && turtleAccess.getDyeColour() != -1){			//鯖側の処理で、タートルが初期色じゃないとき
+		        if (!world.isRemote && turtleAccess.getDyeColour() != -1){      //鯖側の処理で、タートルが初期色じゃないとき
 
-			        turtleAccess.setDyeColour(-1);									//タートルの色を初期に戻す
+			        turtleAccess.setDyeColour(-1);                              //タートルの色を初期に戻す
 
-			        stack.damageItem(1, player);									//アイテムの耐久値を減らす
+			        stack.damageItem(1, player);                                //アイテムの耐久値を減らす
 
-			        return true;													//trueにするとGUIを開かない
+			        return true;                                                //trueにするとGUIを開かない
 		        }
 
 		    } catch (Exception e) {
