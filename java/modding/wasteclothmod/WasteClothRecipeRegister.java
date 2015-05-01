d8cd1266b60a3d7cbe5453186d3d4954e0799b13
neverclear86 <neverclear86sab@gmail.com>
2015-05-01 22:22:30 +0900

    ソース

---
+++ java/modding/wasteclothmod/WasteClothRecipeRegister.java
@@ -0,0 +1,22 @@
+package modding.wasteclothmod;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+public class WasteClothRecipeRegister
+{
+
+	//アイテムレシピ追加
+
+	static void addItemRecipe(){
+
+		GameRegistry.addShapelessRecipe(											/*型なしレシピの追加				*/
+				new ItemStack(WasteClothModCore.itemWasteCloth),					/*出来るアイテム					*/
+				new ItemStack(Blocks.carpet, 1, OreDictionary.WILDCARD_VALUE),		/*カーペット1つ　メタデータは問わない	*/
+				Items.water_bucket													/*水バケツ1つ					*/
+		);
+	}
+}
