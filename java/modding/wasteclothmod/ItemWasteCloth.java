3c891926ec012766bd8c309c413cdfe53ef505cb
neverclear86 <neverclear86sab@gmail.com>
2015-05-01 22:29:07 +0900

    ソース

--- java/modding/wasteclothmod/ItemWasteCloth.java
+++ java/modding/wasteclothmod/ItemWasteCloth.java
@@ -18,14 +18,14 @@ public class ItemWasteCloth extends Item
 	public ItemWasteCloth()
 	{
 		super();
-		this.setCreativeTab(WasteClothModCore.tabWasteCloth)		//クリエタブに追加
-			.setUnlocalizedName("WasteCloth")						//システム名登録
-			.setTextureName("WasteClothMod:waste_cloth")			//テクスチャ登録
-			.setHasSubtypes(true)									//ダメージ値を使う
-			.setMaxDamage(15)										//耐久値設定
-			.setContainerItem(Items.bucket)							//作成時返却アイテム指定
-			.setNoRepair()											//金床使えないよ
-			.setMaxStackSize(1);									//スタックできる数
+		this.setCreativeTab(WasteClothModCore.tabWasteCloth)        //クリエタブに追加
+			.setUnlocalizedName("WasteCloth")                       //システム名登録
+			.setTextureName("WasteClothMod:waste_cloth")            //テクスチャ登録
+			.setHasSubtypes(true)                                   //ダメージ値を使う
+			.setMaxDamage(15)                                       //耐久値設定
+			.setContainerItem(Items.bucket)                         //作成時返却アイテム指定
+			.setNoRepair()                                          //金床使えないよ
+			.setMaxStackSize(1);                                    //スタックできる数
 
 
 	}
@@ -36,8 +36,8 @@ public class ItemWasteCloth extends Item
 		/*onItemUseFirstは右クリック時にブロック側の処理より先に処理される
 		  onItemUseだとブロックの後で処理される	（タートルだとGUIが開く）*/
 
-		TileEntity tile = world.getTileEntity(x, y, z);											/*リフレクション*/
-																								/*無理やりメソッドを呼び出すっぽい*/
+		TileEntity tile = world.getTileEntity(x, y, z);				/*リフレクション*/
+																	/*無理やりメソッドを呼び出すっぽい*/
 		if (tile != null) {
 		    try {
 		        Method methodGetAccess = tile.getClass().getMethod("getAccess");
